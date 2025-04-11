7822815848bed33ae2d11ba3ea92b90579f7ed2f
Keenan Myers <keenan_myers@berkeley.edu>
2025-04-11 11:26:44 -0700

    adding material adding and cx file parsing

--- include/ke.h
+++ include/ke.h
@@ -9,17 +9,28 @@
 class materialManager
 {
   public:
-    std::map<std::string,std::vector<double,double>> m_crossX;
+    // maps material to type of cross section to values (energy,cx)
+    std::map<std::string,std::map<std::string,std::vector<std::pair<double,double>>>> m_crossX;
   public:
-    void addMaterial(std::string,std::vector<double,double>);
-    double getCX(double a_En);
+    // adds given CX from JANIS file
+    void addCX(std::string a_matName,std::string a_CXType,
+                std::string a_fileName);
+    //adds name of material with vector of reaction name and file name
+    void addMaterial(std::string a_matName,
+                    std::vector<std::pair<std::string,std::string>> a_files);
+    // get CX at given energy in material
+    double getCX(std::string a_matName,std::string a_type,double a_En);
+    // sample the distance to interaction
+    double getDist(std::string a_matName,double a_eta);
+    std::string getReactionType(double a_eta,std::string a_matName,
+                                double a_energy);
 }
 // reduces state to tallies and saves them
 class tallies
 {
   public:
-    double m_AvEnergy;
-    double m_
+    double m_avEnergy;
+    double m_avEscape;
 }
 
 #endif
--- src/ke.cpp
+++ src/ke.cpp
@@ -1,2 +1,45 @@
 #include ke.h
-// functions go here
+#include <fstream>
+// puts the class in its basic state
+materialManager::materialManager()
+{
+
+}
+void materialManager::addCX(std::string a_matName,std::string a_CXType,
+                     std::string a_fileName)
+{
+  std::ifstream cxFile(a_fileName);
+  double En;
+  double Cx;
+  getline(cxFile,line)
+  getline(cxFile,line)
+  getline(cxFile,line)
+  while (getline(cxFile,line))
+  {
+    stringstream strSec(line, ';');
+    strSec>>En>>Cx;
+    m_crossX[a_matName][a_CXType].push_back({En,Cx});
+  }
+}
+void materialManager::addMaterial(std::string a_matName,
+                    std::vector<std::pair<std::string,std::string>> a_files)
+{
+
+  for(std::string fi:a_files)
+  {
+    addCX(a_matName,fi.first,fi.second);
+  }
+}
+double materialManager::getCX(std::string a_matName,std::string a_type,double a_En)
+{
+
+}
+double materialManager::getDist(std::string a_matName,double a_eta)
+{
+
+}
+std::string materialManager::getReactionType(double a_eta,std::string a_matName,
+                                double a_energy);
+{
+
+}
