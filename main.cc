47dfb2b0e8dbaf45d25226dfcfca595eebd34452
Keenan Myers <keenan_myers@berkeley.edu>
2025-05-02 16:06:52 -0700

    add material manager intialization and random gen intialization

--- main.cc
+++ main.cc
@@ -28,7 +28,25 @@ int main(int argc, char** argv)
   fs.setFissionSite(neut2,99);
   fs.printFissionSites();
 
+  // begin material intialization
+  materialManager* mat = new materialManager();
+  randomGen* randy = new randomGen();
+  // randomGen* randy = new randomGen(seed);
 
+  // begin geometry pu/fe only
+  mat->addShapeFromFile("./geos/twoCubes.txt");
+  // mat->addShapeFromFile("./geos/incased.txt");
+  // put fe and c
+  // mat->addShapeFromFile("./geos/checkers.txt");
+
+  // add cx data
+  mat->addMaterial("fe",{{"abs","./crossSecs/ironAbs.csv"},{"tot","./crossSecs/ironTot.csv"}});
+  mat->addMaterial("pu",{{"fis","./crossSecs/plutoniumFis.csv"},{"abs","./crossSecs/plutoniumAbs.csv"},{"tot","./crossSecs/plutoniumTot.csv"}});
+  mat->addMaterial("c",{{"abs","./crossSecs/carbonAbs.csv"},{"tot","./crossSecs/carbonTot.csv"}});
+  mat->addNu("pu",2.8836);
+  mat->addDensity("pu",19.86,239);
+  mat->addDensity("fe",7.874,56);
+  // mat->addDensity("c",2.266,12); //graphite
 
 
   return 0;
