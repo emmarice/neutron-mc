de0f92558c007cf352107c72170491cdf9504ffd
Keenan Myers <keenan_myers@berkeley.edu>
2025-05-06 15:53:33 -0700

    fixed MC stat to intake dimensions from file, added proper useage message

--- main.cc
+++ main.cc
@@ -12,39 +12,26 @@
 /*
   The arguments for main will take in order:
   The number of particles to be run
-  The length in x in cm
-  The length in y in cm
   the number of grids per cm
 */
 int main(int argc, char** argv)
 {
-  if(argc!=5)
+  if(argc!=3)
   {
     std::cout<<"invalid arg count"<<std::endl;
+    std::cout<<"correct use should be ./main {num particles} {grid per cm}"<<std::endl;
     return 1;
   }
-  int numpart = atoi(argv[1]);
-  float xdim = atof(argv[2]);
-  float ydim = atof(argv[3]);
-  int grids = atoi(argv[4]);
-  MCstats fs = MCstats(xdim,ydim,grids);
-  neutron neut = neutron(1,3.5,2.6,40);
-  fs.setFissionSite(neut);
-  fs.printFissionSites();
-  neutron neut2 = neutron(1,4.3,1.2,40);
-  fs.setFissionSite(neut2,99);
-  fs.printFissionSites();
-
   // begin material intialization
   materialManager* mat = new materialManager();
   randomGen* randy = new randomGen();
   // randomGen* randy = new randomGen(seed);
 
   // begin geometry pu/fe only
-  mat->addShapeFromFile("../neutron-mc/geos/twoCubes.txt");
-  // mat->addShapeFromFile("./geos/incased.txt");
+  std::pair<double,double> dims = mat->addShapeFromFile("../neutron-mc/geos/twoCubes.txt");
+  // std::pair<double,double> dims = mat->addShapeFromFile("./geos/incased.txt");
   // put fe and c
-  // mat->addShapeFromFile("./geos/checkers.txt");
+  // std::pair<double,double> dims = mat->addShapeFromFile("./geos/checkers.txt");
 
   // add cx data
   mat->addMaterial("fe",{{"abs","../neutron-mc/crossSecs/ironAbs.csv"},{"tot","../neutron-mc/crossSecs/ironTot.csv"}});
@@ -56,6 +43,20 @@ int main(int argc, char** argv)
   // mat->addDensity("c",2.266,12); //graphite
 
   // end material initalization
+
+  // start MC stats
+  int numpart = atoi(argv[1]);
+  float xdim = dims.first;
+  float ydim = dims.second;
+  int grids = atoi(argv[2]);
+  MCstats fs = MCstats(xdim,ydim,grids);
+  neutron neut = neutron(1,3.5,2.6,40);
+  fs.setFissionSite(neut);
+  fs.printFissionSites();
+  neutron neut2 = neutron(1,4.3,1.2,40);
+  fs.setFissionSite(neut2,99);
+  fs.printFissionSites();
+
 
 
   return 0;
